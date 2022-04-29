7785fd598e27f2559d72d6f61d34d128a772a2ca
leconman <Connor.Courtien83@myhunter.cuny.edu>
2022-04-29 16:41:23 -0400

    formatted balance to 2 decimal places

--- src/App.js
+++ src/App.js
@@ -68,7 +68,7 @@ class App extends React.Component {
           <Route path="/" element={<Home />} />
           <Route path="/debits" element={<Debits addDebit={this.addDebit} debits={this.state.debits} />} />
         </Routes>
-        <h3>{this.state.accountBalance}</h3>
+        <h3>{this.state.accountBalance.toFixed(2)}</h3>
       </div>
     );
   }
